ca74386b5f8d89cfb639d87f9375a1c0e158dc56
wlyle473 <wlyle473@gmail.com>
2025-03-25 20:47:14 +0000

    refactor: Moved training pipeline and cleaned imports.

---
+++ src/training/pipeline.py
@@ -0,0 +1,122 @@
+import torch
+import torchvision
+import torch.nn as nn
+import torch.optim as optim
+import os
+import pickle
+
+from sklearn.model_selection import train_test_split
+from torch.optim.lr_scheduler import CosineAnnealingLR
+from typing import Dict, Any, List, Union
+
+from src.utils import load_active_learning_embeddings
+from src.training.engine import create_resnet18_model, train_model, test_model, calculate_metrics
+from src.training.dataset import CustomDataset
+
+class TrainingPipeline:
+
+    def __init__(
+                self, 
+                training_settings:Dict[str, Any],
+                classes:List[str],
+                transform:torchvision.transforms.Compose, 
+                device:Union[str, torch.device]
+                ):
+        self.training_settings = training_settings
+        self.classes = classes
+        self.transform = transform
+        self.device = device
+    
+    def load_data(self, embeddings_dir:str):
+
+        all_images, all_labels, _ = load_active_learning_embeddings(embeddings_dir)
+
+        # Separate into train and validation sets:
+        train_images, val_images, train_labels, val_labels = train_test_split(all_images, all_labels, test_size=0.2, random_state=2004)
+
+        print(f"Number of training images: {len(train_images)}")
+        print(f"Number of validation images: {len(val_images)}")
+
+        # train_set = torchvision.datasets.CIFAR10(root="./data", train=True, download=True, transform=transform)
+        train_set = CustomDataset(images=train_images, labels=train_labels, transform=self.transform)
+        train_dl = torch.utils.data.DataLoader(train_set, batch_size=self.training_settings["batch_size"], shuffle=True, num_workers=0)
+
+        val_set = CustomDataset(images=val_images, labels=val_labels, transform=self.transform)
+        val_dl = torch.utils.data.DataLoader(val_set, batch_size=self.training_settings["batch_size"], shuffle=False, num_workers=0)
+
+        test_set = torchvision.datasets.CIFAR10(root="./data", train=False, download=True, transform=self.transform)
+        test_dl = torch.utils.data.DataLoader(test_set, batch_size=self.training_settings["batch_size"], shuffle=False, num_workers=2)
+
+        return train_dl, val_dl, test_dl
+    
+    def initialise_components(self):
+        model = create_resnet18_model()
+        model = model.to(self.device)
+        criterion = nn.CrossEntropyLoss()
+        optimiser = optim.SGD(model.parameters(), lr=self.training_settings["lr"], momentum=self.training_settings["momentum"], nesterov=self.training_settings["use_nesterov"])
+        scheduler = CosineAnnealingLR(optimiser, T_max=self.training_settings["n_epochs"]) # T_max is the number of epochs
+
+        return model, criterion, optimiser, scheduler
+
+    def execute(self, version:str, setting:str, embeddings_dir:str):
+
+        train_dl, val_dl, test_dl = self.load_data(embeddings_dir=embeddings_dir)
+
+        model, criterion, optimiser, scheduler = self.initialise_components()
+
+        model = train_model(
+                            model=model,
+                            criterion=criterion,
+                            optimiser=optimiser,
+                            scheduler=scheduler,
+                            train_dl=train_dl,
+                            val_dl=val_dl,
+                            num_epochs=self.training_settings["n_epochs"],
+                            classes=self.classes,
+                            device=self.device,
+                            print_interval=100
+                            )
+    
+        # Save model
+        split_dir = embeddings_dir.split("/")
+        iterations_and_b_setting = split_dir[-1]
+        MODEL_SAVE_DIR = "models"
+        os.makedirs(MODEL_SAVE_DIR, exist_ok=True)
+        model_path = f"{MODEL_SAVE_DIR}/resnet18_{version}_{setting}_{iterations_and_b_setting}.pth"
+        torch.save(model.state_dict(), model_path)
+
+        # Test model
+        saved_model = create_resnet18_model()
+        saved_model.load_state_dict(torch.load(model_path))
+        saved_model.to(self.device)
+
+        true_positive, false_positive, false_negative, total = test_model(
+                                                                        model=saved_model,
+                                                                        test_dl=test_dl,
+                                                                        classes=self.classes,
+                                                                        device=self.device
+                                                                        )
+        precision, recall, f1_score, accuracy = calculate_metrics(
+                                                            true_positive=true_positive, 
+                                                            false_positive=false_positive, 
+                                                            false_negative=false_negative, 
+                                                            total=total,
+                                                            classes=self.classes,
+                                                            )
+        total_accuracy = sum(true_positive.values()) / sum(total.values())
+
+        completed_dict = {
+                        "model": saved_model.state_dict(),
+                        "metrics": {
+                                    "precision": precision,
+                                    "recall": recall,
+                                    "f1_score": f1_score,
+                                    "accuracy": accuracy,
+                                    "total_accuracy": total_accuracy
+                                    }
+                        }
+        print("Completed training for version:", version, "and setting:", setting)
+
+        # Save the completed dict in the same location (overwrite)
+        with open(f"{MODEL_SAVE_DIR}/completed_{version}_{setting}_{iterations_and_b_setting}.pkl", "wb") as f:
+            pickle.dump(completed_dict, f)
